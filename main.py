bd78e98a5c8475f72ea5ec19e5a35e2f0bdbf86c
Mr-Tim28 <99376963+Mr-Tim28@users.noreply.github.com>
2022-02-10 10:19:28 +0700

    Update and rename a.py to main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return render_template("home.html")
+  return render_template("homepage.html")
 
 @app.route('/buy/<name>/<price>')
 def buy(name, price):
-  return render_template("buy.html", name=name, price=price)
+  return render_template("buyingpage.html", name=name, price=price)
 
 if __name__ == "__main__":
   app.run(debug=True, port=7000)
